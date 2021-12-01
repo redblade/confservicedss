017ef40d359413200e6e002aa1b2f7ac03094b69
fiadanza <francesco.iadanza@gmail.com>
2021-12-01 09:29:01 +0100

    fix resource msg consumption logs

--- src/main/java/eu/pledgerproject/confservice/PasswordEncode.java
+++ src/main/java/eu/pledgerproject/confservice/PasswordEncode.java
@@ -5,7 +5,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class PasswordEncode {
 
 	public static void main(String[] args) {
-		System.out.println(new BCryptPasswordEncoder().encode(args[0]));
+		String password = args[0];
+		System.out.println("Encoded password for " + password + " is " + (new BCryptPasswordEncoder().encode(password)));
 	}
 
 }
--- src/main/java/eu/pledgerproject/confservice/message/DeserializerResourceMetricDTO.java
+++ src/main/java/eu/pledgerproject/confservice/message/DeserializerResourceMetricDTO.java
@@ -24,54 +24,65 @@ public class DeserializerResourceMetricDTO implements Deserializer<ResourceMetri
 		
 		ResourceMetricDTO result = new ResourceMetricDTO();
 		
-		if(jsonObject.has("DocumentType")) {
+		if(!jsonObject.has("DocumentType")) {
+			log.error("ERROR: resource metric message has no 'DocumentType'. " + source);
+		}
+		else {
 			result.documentType = jsonObject.getString("DocumentType");
 			
 			if( result.documentType.equals(ResourceMetricDTO.DOCUMENT_TYPE_SYSTEM_METRIC) ||
 				result.documentType.equals(ResourceMetricDTO.DOCUMENT_TYPE_APPLICATION_RESOURCE_METRIC)
 			) {
 			
-				String timestampString = jsonObject.getString("TimeStamp");
-				try {
-					timestampString = timestampString.indexOf(".")>0 ? timestampString.substring(0, timestampString.indexOf(".")) : timestampString;
-					result.timestamp = Instant.ofEpochMilli(sdf.parse(timestampString).getTime());
-				}catch(Exception e ) {
-					log.warn("DeserializerResourceMetricDTO. Wrong timestamp format, using now(): got " + timestampString + ", using " + sdf.format(new Date()));
-					result.timestamp = Instant.now();
+				if(!jsonObject.has("TimeStamp")){
+					log.error("ERROR: resource metric message has no 'Timestamp'. " + source);
 				}
+				else {
+					String timestampString = jsonObject.getString("TimeStamp");
+					try {
+						timestampString = timestampString.indexOf(".")>0 ? timestampString.substring(0, timestampString.indexOf(".")) : timestampString;
+						result.timestamp = Instant.ofEpochMilli(sdf.parse(timestampString).getTime());
+					}catch(Exception e ) {
+						log.warn("DeserializerResourceMetricDTO. Wrong timestamp format, using now(): got " + timestampString + ", using " + sdf.format(new Date()));
+						result.timestamp = Instant.now();
+					}
+					
+					try {
+						result.infrastructureName = jsonObject.getString("InfrastructureName");
+						result.infrastructureProvider = jsonObject.getString("InfrastructureProvider");
+						result.nodeName = jsonObject.getString("NodeName");
 				
+						if(result.documentType.equals(ResourceMetricDTO.DOCUMENT_TYPE_SYSTEM_METRIC)) {
+							JSONObject documentBody = jsonObject.getJSONObject("DocumentBody");
+							JSONObject cpu_stats = documentBody.getJSONObject("cpu_stats");
+							result.totalCapacityCpuMillicore = cpu_stats.getDouble("total_millicore");
+							result.totalUsedCpuMillicore = cpu_stats.getDouble("used_millicore");
 				
-				result.infrastructureName = jsonObject.getString("InfrastructureName");
-				result.infrastructureProvider = jsonObject.getString("InfrastructureProvider");
-				result.nodeName = jsonObject.getString("NodeName");
-		
-				if(result.documentType.equals(ResourceMetricDTO.DOCUMENT_TYPE_SYSTEM_METRIC)) {
-					JSONObject documentBody = jsonObject.getJSONObject("DocumentBody");
-					JSONObject cpu_stats = documentBody.getJSONObject("cpu_stats");
-					result.totalCapacityCpuMillicore = cpu_stats.getDouble("total_millicore");
-					result.totalUsedCpuMillicore = cpu_stats.getDouble("used_millicore");
-		
-					JSONObject mem_stats = documentBody.getJSONObject("memory_stats");
-					result.totalCapacityMemoryMB = mem_stats.getDouble("total_megabyte");
-					result.totalUsedMemoryMB = mem_stats.getDouble("used_megabyte");
-				}
-				else if(result.documentType.equals(ResourceMetricDTO.DOCUMENT_TYPE_APPLICATION_RESOURCE_METRIC)) {
-					JSONObject documentBody = jsonObject.getJSONObject("DocumentBody");
-					JSONArray appMetrics = documentBody.getJSONArray("Service");
-					for(int i=0; i < appMetrics.length(); i++) {
-						JSONObject appMetric = appMetrics.getJSONObject(i);
-						
-						String serviceName = appMetric.getString("name");
-						JSONObject cpu_stats = appMetric.getJSONObject("cpu_stats");
-		
-						Double serviceLimitCpuMillicore = cpu_stats.getDouble("limit_millicore");
-						Double serviceUsedCpuMillicore = cpu_stats.getDouble("usage_millicore");
-						
-						JSONObject mem_stats = appMetric.getJSONObject("memory_stats");
-						Double serviceLimitMemoryMB = mem_stats.getDouble("limit_megabyte");
-						Double serviceUsedMemoryMB = mem_stats.getDouble("usage_megabyte");
-						
-						result.appMetrics.add(new ResourceAppMetricDTO(serviceName, serviceLimitCpuMillicore, serviceUsedCpuMillicore, serviceLimitMemoryMB, serviceUsedMemoryMB));
+							JSONObject mem_stats = documentBody.getJSONObject("memory_stats");
+							result.totalCapacityMemoryMB = mem_stats.getDouble("total_megabyte");
+							result.totalUsedMemoryMB = mem_stats.getDouble("used_megabyte");
+						}
+						else if(result.documentType.equals(ResourceMetricDTO.DOCUMENT_TYPE_APPLICATION_RESOURCE_METRIC)) {
+							JSONObject documentBody = jsonObject.getJSONObject("DocumentBody");
+							JSONArray appMetrics = documentBody.getJSONArray("Service");
+							for(int i=0; i < appMetrics.length(); i++) {
+								JSONObject appMetric = appMetrics.getJSONObject(i);
+								
+								String serviceName = appMetric.getString("name");
+								JSONObject cpu_stats = appMetric.getJSONObject("cpu_stats");
+				
+								Double serviceLimitCpuMillicore = cpu_stats.getDouble("limit_millicore");
+								Double serviceUsedCpuMillicore = cpu_stats.getDouble("usage_millicore");
+								
+								JSONObject mem_stats = appMetric.getJSONObject("memory_stats");
+								Double serviceLimitMemoryMB = mem_stats.getDouble("limit_megabyte");
+								Double serviceUsedMemoryMB = mem_stats.getDouble("usage_megabyte");
+								
+								result.appMetrics.add(new ResourceAppMetricDTO(serviceName, serviceLimitCpuMillicore, serviceUsedCpuMillicore, serviceLimitMemoryMB, serviceUsedMemoryMB));
+							}
+						}
+					}catch(Exception e) {
+						log.error("Unable to parse resource metric message " + source, e);
 					}
 				}
 			}
